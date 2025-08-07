4c8f8dc21fc7bbe92fa65d3b15ebdc8a45c3b5f6
abdullohsoatov001 <soatovabdulloh541@gmail.com>
2025-08-07 13:54:30 +0300

    Update js.js

--- js.js
+++ js.js
@@ -4,16 +4,16 @@ let body = document.querySelector('body')
 
 let son = 0
 sendBtn.addEventListener('click', () => {
-    son++
-    let container = document.createElement('div')
-    body.className = "relative"
-    container.className = "items-center flex gap-2 justify-center "
+    let label = document.createElement('span')
+    label.className = "text-[40px] "
     let text = document.createElement('input')
+    son++
+    body.className = "relative"
+    let container = document.createElement('div')
+    container.className = "items-center flex gap-2 justify-center "
     text.type = "checkbox"
     text.className = "w-[50px] h-[50px]"
-    let label = document.createElement('span')
     label.textContent = input.value + "" + "#" + son
-    label.className = "text-[40px]"
     container.prepend(text, label)
     body.prepend(container)
     let xax = document.querySelector('#xax')
@@ -22,18 +22,15 @@ sendBtn.addEventListener('click', () => {
     text.addEventListener('click', () => {
         label.style.textDecoration = "line-through"
     })
+    let h1 = document.querySelector('h1')
+    let night = document.querySelector('#night')
+    night.addEventListener('click', () => {
+        body.className = "bg-black"
+        h1.style.color = "white"
+        label.classList.add("text-white")
+    })
 })
-let h1 = document.querySelector('h1')
-let night = document.querySelector('#night')
-night.addEventListener('click', () => {
-    body.className = "bg-black"
-    h1.style.color = "white"
-    
-})
-let light = document.querySelector('#light')
-light.addEventListener('click', () => {
-    body.classList.add("bg-white")
-    h1.style.color = "black"
-})
+
+
 
 
